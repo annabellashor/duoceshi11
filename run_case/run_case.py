1406e64a3bb7f6173d3a6c4ea78dedacc8de51cd
annabellashor <1055849182@qq.com>
2022-02-11 19:41:29 +0800

    api_auto

--- run_case/run_case.py
+++ run_case/run_case.py
@@ -3,13 +3,19 @@
 from time import strftime
 from utils.HTMLTestRunner3_New import HTMLTestRunner
 import unittest
+import os
+bath_path=os.path.dirname(os.path.dirname(__file__))
+report_path=bath_path+"/"+"report"
+test_case_path=bath_path+"/"+"test_case"
 now=strftime("%Y-%m-%d_%H_%M_%S")
-file=r"C:\python\api_auto\report"+'\\'+now+"api自动化测试报告.html"
+file=report_path+'\\'+now+"api自动化测试报告.html"
 f=open(file,'wb')
-start_dir=r"C:\python\api_auto\test_case"
+start_dir=test_case_path
 discover=unittest.defaultTestLoader.discover(start_dir=start_dir,pattern="test*.py")
 runner=HTMLTestRunner(stream=f,
                       title="api自动化测试报告",
                       description="用例执行情况如下：",
                       tester="小刘")
 runner.run(discover)
+
+# print(bath_path)
